9fb6418d1569f59b04a5f47d2f855e7e42386782
junior-silva-fatec <161388620+junior-silva-fatec@users.noreply.github.com>
2024-08-16 23:11:26 +0000

    20h11

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,21 @@
 import { Text, View, StyleSheet } from "react-native";
+const produtos = [  
+  {id: 1, nome: "Coca-Cola", preco: 5.5},
+  {id: 2, nome: "Pepsi", preco: 5.5},
+  {id: 3, nome: "Fanta", preco: 5.5},
+  {id: 4, nome: "Guaraná", preco: 5.5}
+]
 
 export default function Index() {
   return (
     <View style={estilo.container}
     >
-    <Text style={estilo.text}>Hello World!</Text>
-     
+     {produtos.map((produto) => (
+      <View key={produto.id} style={estilo.lista}>
+        <Text>{produto.nome}</Text>
+        <Text>{produto.preco}</Text>
+      </View>
+      ))}
     </View>
   );
 }
@@ -20,6 +30,10 @@ const estilo = StyleSheet.create(
     },
     text:{
       color: "#FFFFFF"
+    },
+    lista:{
+      display: "flex",
+      flexDirection: "row",      
     }
   }
 )
